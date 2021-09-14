c8ac37a284ae2ceabf685866178af05ba27fc5b9
Adrian Ferrera <adrian.afergon@gmail.com>
2021-09-14 14:01:22 +0200

    🚀 create project

---
+++ src/main/kotlin/dev/afergon/kotlinhibernateperformance/KotlinHibernatePerformanceApplication.kt
@@ -0,0 +1,11 @@
+package dev.afergon.kotlinhibernateperformance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinHibernatePerformanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinHibernatePerformanceApplication>(*args)
+}
---
+++ src/test/kotlin/dev/afergon/kotlinhibernateperformance/KotlinHibernatePerformanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.afergon.kotlinhibernateperformance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinHibernatePerformanceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
